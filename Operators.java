11b0ce97f2b967c8056ebc1790c4e14715438fe8
harshitasharma2709 <harshitasharma5942@gmail.com>
2025-08-12 14:59:58 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,90 @@
+// Arithmatic - An array is a fixed-size collection of elements of the same type
+// arithmatic have 5 opertaors :- addition, subtration, multiplication, division, modulos
+
+// import java.util.*;
+// public class Operators{ 
+    
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         // System.out.println(a+b);
+//         // System.out.println(a-b);
+//         // System.out.println(a*b);
+//         // System.out.println(a/b);
+//         System.out.println(a%b);
+
+//     }
+// }
+
+// import java.util.*;
+// public class Operators{
+//     public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     int a = sc.nextInt();
+//     int b = sc.nextInt();
+//     System.out.println(a+b);
+// }
+// }
+
+// import java.util.*;
+// public class Operators{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         System.out.println(a%b);
+//     }
+// }
+
+// Assignment Operators - are used to assign values to variables.
+// =, +=, -=, *=, 
+
+
+// public class Operators{
+//     public static void main(String[] args){
+//         int a = 3;
+//         b += 2;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// } 
+
+// import java.util.*;
+// public class Operators{
+//     public static void main(String[] args){
+        // int a = 3;
+//         a *= 2;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// } 
+
+// public class Operators{
+//     public static void main(String[] args){
+//         int a = 4;
+//         a /= 2;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// } 
+
+// public class Operators{
+//     public static void main(String[] args){
+//         // Scanner sc = new Scanner(System.in);
+       // // int a = 2;
+//         int b = 4;
+//         b *= 2;
+//         System.out.println(a);
+//         System.out.println(b);
+//     }
+// } 
+
+public class Operators{
+    public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        int b = 7;
+        b -= 2;
+        System.out.println(b);
+    }
+} 
